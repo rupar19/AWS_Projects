224539fce113541ad50a9fda5654b09621972774
Rupa Roy Bhowmick <rupar.19@gmail.com>
2023-06-14 18:07:23 +0100

    Add files via upload

---
+++ ServerlessWebApplication/wildrydes-site/js/config.js
@@ -0,0 +1,11 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-2_HPCj6M54u', 
+        userPoolClientId: 'cqp6i9fqhk58soo0nu0culipg', 
+        region: 'eu-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: ' https://xqu27rcpab.execute-api.eu-west-2.amazonaws.com/prod',
+    }
+};
+ 
